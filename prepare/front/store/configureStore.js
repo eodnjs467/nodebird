9fc43442d9ea28a752feb7f788495940ab8b72bb
Big One <sponjjanc@naver.com>
2023-02-06 22:53:30 +0900

    미들웨어와 리덕스 데브툴즈(개발용, 프로덕션용)

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,12 +1,16 @@
 import {createWrapper} from 'next-redux-wrapper';
 import reducer from '../reducers';
-import {createStore} from "redux";
+import {applyMiddleware, compose, createStore} from "redux";
 import process from "prop-types/prop-types";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 const configureStore = () => {
-    const store = createStore(reducer);
+    const middlewares = [];
+    const enhancer = process.env.NODE_ENV === 'production'
+        ? compose(applyMiddleware(...middlewares))
+        : composeWithDevTools(applyMiddleware(...middlewares));
+    const store = createStore(reducer, enhancer);
     return store;
-
 };
 
 const wrapper = createWrapper(configureStore, {
