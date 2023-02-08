7e18432430b226115220bf72f73fbf056d8d6e28
Big One <sponjjanc@naver.com>
2023-02-08 23:12:35 +0900

    단순 수정

--- prepare/front/pages/index.js
+++ prepare/front/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Head from "next/head";
 import AppLayout from "../components/AppLayout";
 import {useSelector} from "react-redux";
 import PostForm from "../components/PostForm";
