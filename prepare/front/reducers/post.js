2ef6baa8602b9462422bf3c7ac10158287dc28b3
Big One <sponjjanc@naver.com>
2023-02-08 23:12:16 +0900

    게시글 등록, 댓글 등록, 이미지, 이미지 줌 구현

--- prepare/front/reducers/post.js
+++ prepare/front/reducers/post.js
@@ -5,9 +5,13 @@ export const initialState = {
             id: 1,
             nickname: '큰 동그라미',
         },
-        content: '첫 번쨰 게시글 #해시태그 #익스프레스',
+        content: '첫 번째 게시글 #해시태그 #익스프레스',
         Images: [{
-            src: 'https://'
+            src: "https://books.google.co.kr/books/publisher/content?id=bU-kEAAAQBAJ&hl=ko&pg=PA1&img=1&zoom=3&sig=ACfU3U1LwicLa4geYtfPq8qPJI8ojzxVOA&w=1280",
+        }, {
+            src: "https://books.google.co.kr/books/publisher/content?id=7GlUDwAAQBAJ&hl=ko&pg=PA1&img=1&zoom=3&sig=ACfU3U113cBAVbTTveUsf2ZkNxQhviUhyA&w=1280",
+        }, {
+            src: "https://books.google.co.kr/books/publisher/content?id=7GlUDwAAQBAJ&hl=ko&pg=PA1&img=1&zoom=3&sig=ACfU3U113cBAVbTTveUsf2ZkNxQhviUhyA&w=1280",
         }],
         Comments: [{
             User: {
@@ -26,23 +30,44 @@ export const initialState = {
 };
 
 const ADD_POST = 'ADD_POST';
-export const addPost = {
-    type: ADD_POST,
-}
-const dummyPost = {
-    id: 2,
-    content: '은중 데이터입니다.',
-    Image:[],
-    Comment: [],
-}
+const ADD_COMMENT = 'ADD_COMMENT';
+export const addPost = data => {
+    return {
+        type: ADD_POST,
+        data,
+    }
+};
 
+export const addComment = data => {
+    console.log('data: ', data)
+    return{
+        type: ADD_COMMENT,
+        data,
+    }
+}
 const reducer = (state=initialState, action) => {
     switch (action.type){
         case ADD_POST:
             return {
                 ...state,
-                mainPosts: [dummyPost, ...state.mainPosts],
+                mainPosts: [{
+                    id: state.mainPosts.length+1,
+                    User: {
+                        id: action.data.id,
+                        nickname: '임시 닉네임입니다.',
+                    },
+                    content: action.data.content,
+                    Images: [],
+                    Comments: [],
+                }, ...state.mainPosts],
                 postAdded: true,
+            };
+        case ADD_COMMENT:
+            return {
+                ...state,
+                mainPosts: state.mainPosts.map(post => {
+                    return post.id === action.data.id ? {...post, Comments: [{User: {nickname: '임시용~'}, content: action.data.content}, ...post.Comments]} : post
+                }),
             };
         default:
             return state;
