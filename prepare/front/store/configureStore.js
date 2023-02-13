60aa4163f2fe1586b84b921697b78981f00be33c
Big One <sponjjanc@naver.com>
2023-02-13 19:20:28 +0900

    redux-saga 미들웨어 적용

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -3,7 +3,9 @@ import reducer from '../reducers';
 import {applyMiddleware, compose, createStore} from "redux";
 import process from "prop-types/prop-types";
 import {composeWithDevTools} from "redux-devtools-extension";
-import thunkMiddleware from 'redux-thunk';
+// import thunkMiddleware from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from '../sagas';
 
 const loggerMiddleware = ({dispatch, getState}) => (next) => (action) => {
     console.log(action);
@@ -11,11 +13,13 @@ const loggerMiddleware = ({dispatch, getState}) => (next) => (action) => {
 }
 
 const configureStore = () => {
-    const middlewares = [thunkMiddleware, loggerMiddleware];
+    const sagaMiddleware = createSagaMiddleware();
+    const middlewares = [sagaMiddleware, loggerMiddleware];
     const enhancer = process.env.NODE_ENV === 'production'
         ? compose(applyMiddleware(...middlewares))
         : composeWithDevTools(applyMiddleware(...middlewares));
     const store = createStore(reducer, enhancer);
+    store.sagaTask = sagaMiddleware.run(rootSaga);
     return store;
 };
 
