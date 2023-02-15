c86611156b96cab6568fefc62c8407537e9d2041
Big One <sponjjanc@naver.com>
2023-02-15 20:05:30 +0900

    Feat: 회원가입, 게시글, 댓글

--- prepare/front/sagas/post.js
+++ prepare/front/sagas/post.js
@@ -1,30 +1,54 @@
-import axios from "axios";
-import {all, fork, put, takeLatest, delay} from 'redux-saga/effects';
+import axios from 'axios';
+import { all, fork, put, takeLatest, delay } from 'redux-saga/effects';
+import { ADD_COMMENT_SUCCESS, ADD_COMMENT_FAILURE, ADD_POST_SUCCESS, ADD_POST_FAILURE } from '../reducers/post';
 
-function addPostAPI(data){
-    return axios.post('/api/addPost', data);
+function addPostAPI(data) {
+  return axios.post('/api/addPost', data);
 }
 
 function* addPost(action) {
-    try {
-        // const result = yield all(addPostAPI, action.data)
-        yield delay(1000);
-        yield put({
-            type: 'ADD_POST_SUCCESS',
-            // data: result.data,
-        })
-    } catch (err) {
-        yield put({
-            type: 'ADD_POST_FAILURE',
-            data: err.response.data,
-        });
-    }
+  try {
+    // const result = yield all(addPostAPI, action.data)
+    yield delay(1000);
+    yield put({
+      type: ADD_POST_SUCCESS,
+      // data: result.data,
+      data: action.data,
+    });
+  } catch (err) {
+    yield put({
+      type: ADD_POST_FAILURE,
+      error: err.response.data,
+    });
+  }
 }
-function* watchAddPost(){
-    yield takeLatest('ADD_POST_REQUEST', addPost);
+
+function* addComment(action) {
+  try {
+    // const result = yield call(addCommentAPI, action.data);
+    delay(1000);
+    yield put({
+      type: ADD_COMMENT_SUCCESS,
+      data: action.data,
+    })
+  } catch (err) {
+    yield put({
+      type: ADD_COMMENT_FAILURE,
+      err: err.response.data,
+    });
+  }
 }
-export default function* postSaga(){
-    yield all([
-        fork(watchAddPost),
-    ]);
+
+function* watchAddPost() {
+  yield takeLatest('ADD_POST_REQUEST', addPost);
+}
+
+function* watchAddComment() {
+  yield takeLatest('ADD_COMMENT_REQUEST', addComment);
+}
+export default function* postSaga() {
+  yield all([
+    fork(watchAddPost),
+    fork(watchAddComment),
+  ]);
 }
--- prepare/front/sagas/user.js
+++ prepare/front/sagas/user.js
@@ -1,5 +1,10 @@
 import axios from "axios";
-import {all, fork, put, delay, takeLatest} from 'redux-saga/effects';
+import { all, fork, put, delay, takeLatest } from 'redux-saga/effects';
+import {
+    LOG_IN_FAILURE, LOG_IN_REQUEST, LOG_IN_SUCCESS,
+    LOG_OUT_FAILURE, LOG_OUT_REQUEST, LOG_OUT_SUCCESS,
+    SIGN_UP_FAILURE, SIGN_UP_REQUEST, SIGN_UP_SUCCESS,
+} from "../reducers/user";
 
 function logInAPI(data) {
     return axios.post('/api/login', data);
@@ -9,20 +14,24 @@ function logOutAPI() {
     return axios.post('/api/logout');
 }
 
-function* logIn(action){
-    try{
+function signUpAPI(data) {
+    return axios.post('/api/signup/', data);
+}
+
+function* logIn(action) {
+    try {
         // const result = yield call(logInAPI, action.data);
         yield delay(1000);
         yield put({
-            type: 'LOG_IN_SUCCESS',
-            data: action.data
+            type: LOG_IN_SUCCESS,
+            data: action.data,
             // data: result.data,
         });
-    }catch (err){
+    } catch (err) {
         yield put({
-            type:'LOG_IN_FAILURE',
-            data: err.response.data,
-        })
+            type: LOG_IN_FAILURE,
+            error: err.response.data,
+        });
     }
 }
 
@@ -31,28 +40,49 @@ function* logOut() {
         // const result = yield call(logOutAPI);
         yield delay(1000);
         yield put({
-            type: 'LOG_OUT_SUCCESS',
+            type: LOG_OUT_SUCCESS,
             // data: result.data,
-        })
+        });
     } catch (err) {
         yield put({
-            type: 'LOG_OUT_FAILURE',
-            data: err.response.data,
+            type: LOG_OUT_FAILURE,
+            error: err.response.data,
         });
     }
 }
 
-function* watchLogIn(){
-    yield takeLatest('LOG_IN_REQUEST', logIn);      // type: 'LOG_IN_REQUEST' 과 이거에 필요한 data 가 action 이라는거에 있는데 그게 logIn()에 매개변수로 전달된다.
+function* signUp(action) {
+    try {
+        // const result = yield call(signUpAPI, action.data);
+        yield delay(1000);
+        yield put({
+            type: SIGN_UP_SUCCESS,
+            data: action.data,
+        });
+    } catch (err) {
+        yield put({
+            type: SIGN_UP_FAILURE,
+            err: err.response.data,
+        });
+    }
 }
 
-function* watchLogOut(){
-    yield takeLatest('LOG_OUT_REQUEST', logOut);
+function* watchLogIn() {
+    yield takeLatest(LOG_IN_REQUEST, logIn); // type: 'LOG_IN_REQUEST' 과 이거에 필요한 data 가 action 이라는거에 있는데 그게 logIn()에 매개변수로 전달된다.
 }
 
-export default function* userSaga(){
+function* watchLogOut() {
+    yield takeLatest(LOG_OUT_REQUEST, logOut);
+}
+
+function* watchSignUP() {
+    yield takeLatest(SIGN_UP_REQUEST, signUp);
+}
+
+export default function* userSaga() {
     yield all([
         fork(watchLogIn),
         fork(watchLogOut),
+        fork(watchSignUP),
     ]);
 }
