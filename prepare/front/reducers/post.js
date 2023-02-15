925bddd41baf310b2efe65367825b7ce41c4d7cb
Big One <sponjjanc@naver.com>
2023-02-15 20:06:46 +0900

    Feat: 회원가입, 게시글, 댓글

--- prepare/front/reducers/post.js
+++ prepare/front/reducers/post.js
@@ -1,77 +1,121 @@
 export const initialState = {
-    mainPosts: [{
-        id: 1,
-        User: {
-            id: 1,
-            nickname: '큰 동그라미',
-        },
-        content: '첫 번째 게시글 #해시태그 #익스프레스',
-        Images: [{
-            src: "https://books.google.co.kr/books/publisher/content?id=bU-kEAAAQBAJ&hl=ko&pg=PA1&img=1&zoom=3&sig=ACfU3U1LwicLa4geYtfPq8qPJI8ojzxVOA&w=1280",
-        }, {
-            src: "https://books.google.co.kr/books/publisher/content?id=7GlUDwAAQBAJ&hl=ko&pg=PA1&img=1&zoom=3&sig=ACfU3U113cBAVbTTveUsf2ZkNxQhviUhyA&w=1280",
-        }, {
-            src: "https://books.google.co.kr/books/publisher/content?id=7GlUDwAAQBAJ&hl=ko&pg=PA1&img=1&zoom=3&sig=ACfU3U113cBAVbTTveUsf2ZkNxQhviUhyA&w=1280",
-        }],
-        Comments: [{
-            User: {
-                nickname: 'Dho',
-            },
-            content: '우와 바나나가 있군요~',
-        }, {
-            User: {
-                nickname: 'SilverCenter',
-            },
-            content: '우와 오늘도 개드립을 치는군요~',
-        }]
+  mainPosts: [{
+    id: 1,
+    User: {
+      // id: 'dummyUser',
+      id: 'admin',
+      nickname: '큰 동그라미',
+    },
+    content: '첫 번째 게시글 #해시태그 #익스프레스',
+    Images: [{
+      src: 'https://books.google.co.kr/books/publisher/content?id=bU-kEAAAQBAJ&hl=ko&pg=PA1&img=1&zoom=3&sig=ACfU3U1LwicLa4geYtfPq8qPJI8ojzxVOA&w=1280',
+    }, {
+      src: 'https://books.google.co.kr/books/publisher/content?id=7GlUDwAAQBAJ&hl=ko&pg=PA1&img=1&zoom=3&sig=ACfU3U113cBAVbTTveUsf2ZkNxQhviUhyA&w=1280',
+    }, {
+      src: 'https://books.google.co.kr/books/publisher/content?id=7GlUDwAAQBAJ&hl=ko&pg=PA1&img=1&zoom=3&sig=ACfU3U113cBAVbTTveUsf2ZkNxQhviUhyA&w=1280',
     }],
-    imagePaths: [],
-    postAdded: false,
+    Comments: [{
+      User: {
+        nickname: 'Dho',
+      },
+      content: '우와 바나나가 있군요~',
+    }, {
+      User: {
+        nickname: 'SilverCenter',
+      },
+      content: '우와 오늘도 개드립을 치는군요~',
+    }],
+  }],
+  imagePaths: [],
+  addPostLoading: false,
+  addPostDone: false,
+  addPostError: null,
+  addCommentLoading: false,
+  addCommentDone: false,
+  addCommentError: null,
 };
 
-const ADD_POST = 'ADD_POST';
-const ADD_COMMENT = 'ADD_COMMENT';
-export const addPost = data => {
-    return {
-        type: ADD_POST,
-        data,
-    }
-};
+export const ADD_POST_REQUEST = 'ADD_POST_REQUEST';
+export const ADD_POST_SUCCESS = 'ADD_POST_SUCCESS';
+export const ADD_POST_FAILURE = 'ADD_POST_FAILURE';
 
-export const addComment = data => {
-    console.log('data: ', data)
-    return{
-        type: ADD_COMMENT,
-        data,
-    }
-}
-const reducer = (state=initialState, action) => {
-    switch (action.type){
-        case ADD_POST:
-            return {
-                ...state,
-                mainPosts: [{
-                    id: state.mainPosts.length+1,
-                    User: {
-                        id: action.data.id,
-                        nickname: '임시 닉네임입니다.',
-                    },
-                    content: action.data.content,
-                    Images: [],
-                    Comments: [],
-                }, ...state.mainPosts],
-                postAdded: true,
-            };
-        case ADD_COMMENT:
-            return {
-                ...state,
-                mainPosts: state.mainPosts.map(post => {
-                    return post.id === action.data.id ? {...post, Comments: [{User: {nickname: '임시용~'}, content: action.data.content}, ...post.Comments]} : post
-                }),
-            };
-        default:
-            return state;
-    }
-}
+export const ADD_COMMENT_REQUEST = 'ADD_COMMENT_REQUEST';
+export const ADD_COMMENT_SUCCESS = 'ADD_COMMENT_SUCCESS';
+export const ADD_COMMENT_FAILURE = 'ADD_COMMENT_FAILURE';
+export const addPostRequest = (data) => ({
+  type: ADD_POST_REQUEST,
+  data,
+});
+
+export const addCommentRequest = (data) => ({
+  type: ADD_COMMENT_REQUEST,
+  data,
+});
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_POST_REQUEST:
+      return {
+        ...state,
+        addPostLoading: true,
+        addPostDone: false,
+        addPostError: null,
+      };
+    case ADD_POST_SUCCESS:
+      return {
+        ...state,
+        mainPosts: [{
+          id: state.mainPosts.length + 1,
+          User: {
+            id: action.data.userId,
+            nickname: action.data.nickname,
+          },
+          content: action.data.content,
+          Images: [],
+          Comments: [],
+        }, ...state.mainPosts],
+        addPostLoading: false,
+        addPostDone: true,
+      };
+    case ADD_POST_FAILURE:
+      return {
+        ...state,
+        addPostLoading: false,
+        addPostError: action.error,
+      };
+    case ADD_COMMENT_REQUEST:
+      return {
+        ...state,
+        addCommentLoading: true,
+        addCommentDone: false,
+        addCommentError: null,
+      };
+    case ADD_COMMENT_SUCCESS:
+      return {
+        ...state,
+        mainPosts: state.mainPosts.map((post) => (
+          post.id === action.data.postId
+            ? {
+              ...post,
+              Comments: [{
+                User: { id: action.data.user.userId, nickname: action.data.user.nickname },
+                content: action.data.content },
+              ...post.Comments] }
+            : post
+        )),
+        addCommentLoading: false,
+        addCommentDone: true,
+      };
+    case ADD_COMMENT_FAILURE:
+      return {
+        ...state,
+        addCommentLoading: false,
+        addCommentError: action.error,
+      };
+
+    default:
+      return state;
+  }
+};
 
 export default reducer;
--- prepare/front/reducers/user.js
+++ prepare/front/reducers/user.js
@@ -1,67 +1,139 @@
-export const initialState={
-    isLoggingIn: false, // 로그인 진행중
-    isLoggedIn: false,
-    isLoggingOut: false,//로그아웃 진행중
-    me: null,
-    signUpData: {},
-    loginData:{},
-}
-
-export const loginRequestAction = data => {
-    return {
-        type: 'LOG_IN_REQUEST',
-        data,
-    }
-};
-export const logoutRequestAction = () => {
-    return {
-        type: 'LOG_OUT_REQUEST',
-    }
+export const initialState = {
+  logInLoading: false, // 로그인 시도
+  logInDone: false,
+  logInError: null,
+  logOutLoading: false, // 로그아웃 시도
+  logOutDone: false,
+  logOutError: null,
+  signUpLoading: false, // 회원가입 시도
+  signUpDone: false,
+  signUpError: null,
+  me: null,
+  signUpData: {},
+  loginData: {},
 };
 
-const reducer = (state=initialState, action) => {
-    switch (action.type) {
-        case 'LOG_IN_REQUEST':
-            return {
-                ...state,
-                isLoggingIn: true,
-            };
-        case 'LOG_IN_SUCCESS':
-            return {
-                ...state,
-                isLoggingIn: false,
-                isLoggedIn: true,
-                me: {...action.data, nickname: 'BigOne'},
-            };
-        case 'LOG_IN_FAILURE':
-            return {
-                ...state,
-                isLoggingIn: false,
-                isLoggedIn: false,
-            };
-        case 'LOG_OUT_REQUEST':
-            return {
-                ...state,
-                isLoggingOut: true,
-            };
-        case 'LOG_OUT_SUCCESS':
-            return {
-                ...state,
-                isLoggingOut: false,
-                isLoggedIn: false,
-                me: null,
-            };
-        case 'LOG_OUT_FAILURE':
-            return {
-                ...state,
-                isLoggingOut: false,
-            };
+const dummyUser = (data) => ({
+  // action.data.id & pw
+  ...data,
+  nickname: 'BigOne',
+  id: 1,
+  Posts: [], // 시퀄라이저에서 합쳐주기때문에 대문자
+  Followings: [],
+  Followers: [],
+});
 
-        default:
-            return state;
-    }
+export const LOG_IN_REQUEST = 'LOG_IN_REQUEST';
+export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
+export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
 
+export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST';
+export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS';
+export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE';
 
-}
+export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
+export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
+export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
+
+export const FOLLOW_REQUEST = 'FOLLOW_REQUEST';
+export const FOLLOW_SUCCESS = 'FOLLOW_SUCCESS';
+export const FOLLOW_FAILURE = 'FOLLOW_FAILURE';
+
+export const UNFOLLOW_REQUEST = 'UNFOLLOW_REQUEST';
+export const UNFOLLOW_SUCCESS = 'UNFOLLOW_SUCCESS';
+export const UNFOLLOW_FAILURE = 'UNFOLLOW_FAILURE';
+
+export const loginRequestAction = (data) => ({
+  type: LOG_IN_REQUEST,
+  data,
+});
+export const logoutRequestAction = () => ({
+  type: LOG_OUT_REQUEST,
+});
+
+export const signUpRequestAction = (data) => {
+  console.log(data);
+  return {
+    type: SIGN_UP_REQUEST,
+    data,
+  };
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOG_IN_REQUEST:
+      return {
+        ...state,
+        logInLoading: true,
+        loginDone: false,
+        loginError: null,
+      };
+    case LOG_IN_SUCCESS:
+      return {
+        ...state,
+        logInLoading: false,
+        loginDone: true,
+        me: dummyUser(action.data),
+      };
+    case LOG_IN_FAILURE:
+      return {
+        ...state,
+        logInLoading: false,
+        logInError: action.error,
+      };
+    case LOG_OUT_REQUEST:
+      return {
+        ...state,
+        logOutLoading: true,
+        logOutDone: false,
+        logOutError: null,
+      };
+    case LOG_OUT_SUCCESS:
+      return {
+        ...state,
+        logOutLoading: false,
+        logOutDone: true,
+        me: null,
+      };
+    case LOG_OUT_FAILURE:
+      return {
+        ...state,
+        logOutLoading: false,
+        logOutError: action.error,
+      };
+    case SIGN_UP_REQUEST:
+      return {
+        ...state,
+        signUpLoading: true,
+        signUpError: null,
+        signUpDone: false,
+      };
+    case SIGN_UP_SUCCESS:
+      return {
+        ...state,
+        signUpLoading: false,
+        signUpDone: true,
+        signUpData: {
+          ...state.signUpData,
+          id: dummyUser.id + 1, // TODO: 수정요망
+          email: action.data.email,
+          nickname: action.data.nickname,
+          password: action.data.password, // TODO: DB 연결시 지우기
+          Posts: [],
+          Followings: [],
+          Followers: [],
+        },
+      };
+    case SIGN_UP_FAILURE:
+      return {
+        ...state,
+        signUpLoading: false,
+        signUpError: action.error,
+      };
+
+    default:
+      return state;
+  }
+};
 
 export default reducer;
