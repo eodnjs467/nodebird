5e57ef93f14c1ae2408c675e8ae22fbb425cff3f
Big One <sponjjanc@naver.com>
2023-02-24 00:45:10 +0900

    fix: password 글자 수 제한 수정

--- prepare/back/models/user.js
+++ prepare/back/models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false,
         },
         password: {
-            type: DataTypes.STRING(20),
+            type: DataTypes.STRING(100),
             allowNull: false,
         },
     }, {
