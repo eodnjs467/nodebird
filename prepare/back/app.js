b509e432a33d4bf7a60ded1e5a01df7fecb4e8f7
Big One <sponjjanc@naver.com>
2023-02-24 00:46:36 +0900

    Feat: cors, passport

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const postRouter = require('./routes/post');
+const userRouter = require('./routes/user');
 const db = require('./models');
 const app = express();
+const passportConfig = require('./passport');
 
 db.sequelize.sync()
     .then(() => {
@@ -9,19 +12,21 @@ db.sequelize.sync()
     })
     .catch(console.error);
 
-app.get('/', (req, res) => {
-    res.send('hello express!');
-});
+passportConfig();
 
-app.post('/post', (req, res) => {
-    res.json({id: 1, content: 'hello'});
-});
+app.use(cors({
+    origin: '*',
+    credentials: false,
+}));
+app.use(express.json());        // 프론트에서 json 형식으로 보냈을 때 json 형식을 넣어주고
+app.use(express.urlencoded({extended: true}));      // form 데이터를 넣었을 때 urlencoded 방식으로 넣어줌 이두개가 프론트에서 받은 데이터를 req.body 에 넣어주는 역할을 해준다.
 
-app.delete('/post', (req, res) => {
-    res.json({id: 1});
-});
-// app.use('post', postRouter);
+// app.get('/', (req, res) => {
+//     res.send('hello express!');
+// });
+app.use('/post', postRouter);
+app.use('/user', userRouter);
 
-app.listen(3066, () => {
+app.listen(3065, () => {
     console.log('서버 실행 중...');
 });
