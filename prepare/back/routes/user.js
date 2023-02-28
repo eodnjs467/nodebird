000bf23b0682c5ce123536640825b478dbcdd168
Big One <sponjjanc@naver.com>
2023-02-28 21:45:38 +0900

    Feat: 로그인 반환정보, 로그아웃

--- prepare/back/routes/user.js
+++ prepare/back/routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
-const {User} = require('../models');
+const {User, Post} = require('../models');
+const {isLoggedIn, isNotLoggedIn} = require('./middlewares');
 
 const router = express.Router();
 
-router.post('/login', (req, res, next) => {
+router.post('/login', isNotLoggedIn, (req, res, next) => {
     passport.authenticate('local', (err, user, info) => {
         //  err 이 있다는 건 비동기요청한 서버에러가 발생했다.
         if(err){
@@ -24,13 +25,28 @@ router.post('/login', (req, res, next) => {
                 console.log(loginError);
                 return next(loginError);
             }
+            const fullUserWithoutPassword = await User.findOne({
+                where: {id: user.id},
+                attributes: {
+                    exclude: ['password'],
+                },
+                include: [{
+                    model: Post,
+                }, {
+                    model: User,
+                    as: 'Followers',
+                }, {
+                    model: User,
+                    as: 'Followings',
+                }]
+            });
             // 내가 만든 서비스 통과 후, 패스포트 로그인 통과해서 진짜 최종 로그인 성공이라 user 정보 넘겨줌.
-            return res.json(user);
+            return res.status(200).json(fullUserWithoutPassword);
         })
     })(req, res, next);
 });
 
-router.post('/', async (req, res, next) => {
+router.post('/', isNotLoggedIn, async (req, res, next) => {
     try{
         const exUser = await User.findOne({
             where: {
@@ -45,7 +61,7 @@ router.post('/', async (req, res, next) => {
             email: req.body.email,
             nickname: req.body.nickname,
             password: hashedPassword,
-        })
+        });
         // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3060');
         res.status(201).send('OK');
     }catch (error) {
@@ -55,5 +71,14 @@ router.post('/', async (req, res, next) => {
     }
 
 })
+
+router.post('/logout', isLoggedIn, async (req, res, next) => {
+    await req.logout(err => {
+        if(err) {
+            return next(err);
+        }
+        res.send('ok');
+    })
+});
 
 module.exports = router;
