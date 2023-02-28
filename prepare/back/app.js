4209b6bed8518d1f3d683eeff3cacfd9f90f1355
Big One <sponjjanc@naver.com>
2023-02-28 21:41:21 +0900

    Feat: dotenv, express-session

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+const passport = require('passport');
 const cors = require('cors');
 const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
 const db = require('./models');
-const app = express();
+
+const dotenv = require('dotenv');
 const passportConfig = require('./passport');
 
+dotenv.config();
+const app = express();
 db.sequelize.sync()
     .then(() => {
         console.log('db 연결 성공');
     })
     .catch(console.error);
-
-
-
 passportConfig();
 
 app.use(cors({
@@ -24,8 +25,12 @@ app.use(cors({
 }));
 app.use(express.json());        // 프론트에서 json 형식으로 보냈을 때 json 형식을 넣어주고
 app.use(express.urlencoded({extended: true}));      // form 데이터를 넣었을 때 urlencoded 방식으로 넣어줌 이두개가 프론트에서 받은 데이터를 req.body 에 넣어주는 역할을 해준다.
-app.use(session());
-app.use(cookieParser());
+app.use(session({
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.COOKIE_SECRET,
+}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(passport.initialize());
 app.use(passport.session());
 
