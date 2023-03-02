8fb1b45049bb17d89b547a67b99755dee85b883e
Big One <sponjjanc@naver.com>
2023-03-02 22:43:53 +0900

    Feat: 피드(게시글) 작성, 댓글 등록, 미들웨어 적용

--- prepare/back/routes/post.js
+++ prepare/back/routes/post.js
@@ -1,12 +1,67 @@
 const express = require('express');
-
+const {Post, User, Comment, Image} = require('../models');
+const {isLoggedIn} = require("./middlewares");
 const router = express.Router();
-router.post('/', (req, res) => {
-    res.json({id: 1, content: 'hello'});
+router.post('/', isLoggedIn, async (req, res, next) => {
+    console.log('req.user: ', req.user);
+    try{
+        const post = await Post.create({
+            content: req.body.content,
+            UserId: req.user.id,성
+        });
+
+        const fullPost = await Post.findOne({
+            where: {id: post.id},
+            include: [{
+                model: Image,
+            }, {
+                model: Comment,
+                include: [{
+                    attributes: ['id', 'email', 'nickname'],
+                }],
+            }, {
+                model: User,
+                attributes: ['id', 'email', 'nickname'],
+            }],
+        });
+        res.status(201).json(fullPost);
+    }catch (error){
+        console.log(error);
+        next(error);
+    }
+
 });
 
-router.delete('/', (req, res) => {
-    res.json({id: 1});
+router.post(`/:postId/comment`, isLoggedIn, async (req, res, next) => {
+    try{
+        console.log('back:  ', req.params.postId);
+        const post = await Post.findOne({
+            where: {id: req.params.postId},
+        });
+        if(!post){
+            return res.status(403).send('존재하지 않는 게시글입니다.');
+        }
+        const comment = await Comment.create({
+            content: req.body.content,
+            PostId: parseInt(req.params.postId),  //  req.params 는 무조건 문자열임
+            UserId: req.user.id,
+        });
+        const fullComment = await Comment.findOne({
+            where: {id: comment.id},
+            include: [{
+                model: User,
+                attributes: ['id', 'email', 'nickname'],
+            }],
+        });
+        res.status(201).json(fullComment);
+    }catch (error){
+        console.log(error);
+        next(error);
+    }
+})
+
+router.delete('/', isLoggedIn, async (req, res, next) => {
+    res.json({id: req.body.postId});
 });
 
 module.exports = router;
