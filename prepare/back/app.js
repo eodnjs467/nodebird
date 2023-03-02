add7903c7d6884d5c8a7d54eb30772fd90bfdac2
Big One <sponjjanc@naver.com>
2023-03-02 22:42:19 +0900

    Feat: cors credentials 설정으로 쿠키 공유, morgan 연결, posts 라우터 추가

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -4,10 +4,12 @@ const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const cors = require('cors');
 const postRouter = require('./routes/post');
+const postsRouter = require('./routes/posts');
 const userRouter = require('./routes/user');
 const db = require('./models');
 
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 const passportConfig = require('./passport');
 
 dotenv.config();
@@ -18,10 +20,10 @@ db.sequelize.sync()
     })
     .catch(console.error);
 passportConfig();
-
+app.use(morgan('dev'));
 app.use(cors({
-    origin: '*',
-    credentials: false,
+    origin: true,
+    credentials: true,
 }));
 app.use(express.json());        // 프론트에서 json 형식으로 보냈을 때 json 형식을 넣어주고
 app.use(express.urlencoded({extended: true}));      // form 데이터를 넣었을 때 urlencoded 방식으로 넣어줌 이두개가 프론트에서 받은 데이터를 req.body 에 넣어주는 역할을 해준다.
@@ -38,6 +40,7 @@ app.use(passport.session());
 //     res.send('hello express!');
 // });
 app.use('/post', postRouter);
+app.use('/posts', postsRouter);
 app.use('/user', userRouter);
 
 app.listen(3065, () => {
