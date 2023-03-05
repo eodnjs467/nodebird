4ca2fbf9b5c2062b9182e54375765656a2b18e28
Big One <sponjjanc@naver.com>
2023-03-05 21:56:59 +0900

    Feat: 본인 팔로우 제한

--- prepare/front/components/FollowButton.js
+++ prepare/front/components/FollowButton.js
@@ -1,10 +1,10 @@
-import React, {useCallback, useState} from "react";
+import React, { useCallback } from "react";
 import PropTypes from "prop-types";
-import {Button} from "antd";
-import {useDispatch, useSelector} from "react-redux";
+import { Button } from "antd";
+import { useDispatch, useSelector } from "react-redux";
 import { FOLLOW_REQUEST, UNFOLLOW_REQUEST } from "../reducers/user";
 
-const FollowButton = ({post}) => {
+const FollowButton = ({ post }) => {
     const dispatch = useDispatch();
     const { me, unfollowLoading, followLoading } = useSelector((state) => state.user);
     const isFollowing = me?.Followings.find((v) => v.id === post.User.id);
@@ -23,10 +23,10 @@ const FollowButton = ({post}) => {
         }
     }, [isFollowing]);
 
+    if (post.User.id === me.id) return;
+
     return (
-        <>
-            <Button loading={followLoading || unfollowLoading} onClick={onToggleFollow}>{isFollowing ? '언팔로우' : '팔로우'}</Button>
-        </>
+      <Button loading={followLoading || unfollowLoading} onClick={onToggleFollow}>{isFollowing ? '언팔로우' : '팔로우'}</Button>
     );
 };
 
