278bb39782509f640bae8aebe8b8158565640dfc
Big One <sponjjanc@naver.com>
2023-03-10 18:01:21 +0900

    Feat: lastId 로 데이터 이어서 불러오기(페이징처리)

--- prepare/back/routes/posts.js
+++ prepare/back/routes/posts.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const {Post, User, Image, Comment} = require('../models');
+const {Op} = require('sequelize');
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
     try{
+        const where = {};
+        // 초기 로딩이 아닐때
+        if (parseInt(req.query.lastId, 10)) {
+            where.id = {[Op.lt]: parseInt(req.query.lastId, 10)};
+        }
         const posts = await Post.findAll({
+            where,
             limit: 10,
             order: [
                 ['createdAt', 'DESC'],
