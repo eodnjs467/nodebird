8d4902f67567384f778299d2603e85f9f3d42e8f
Big One <sponjjanc@naver.com>
2023-03-14 23:45:36 +0900

    Feat: 해시태그 라우터 추가

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const postRouter = require('./routes/post');
 const postsRouter = require('./routes/posts');
 const userRouter = require('./routes/user');
+const hashtagRouter = require('./routes/hashtag');
 const db = require('./models');
 
 const dotenv = require('dotenv');
@@ -44,6 +45,8 @@ app.use(passport.session());
 app.use('/post', postRouter);
 app.use('/posts', postsRouter);
 app.use('/user', userRouter);
+app.use('/hashtag', hashtagRouter);
+
 
 app.listen(3065, () => {
     console.log('서버 실행 중...');
